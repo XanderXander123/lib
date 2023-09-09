ecf8bf9c983c31aaddf020978b83807b76db8c80
_x128 <143216072+XanderXander123@users.noreply.github.com>
2023-09-09 07:57:47 -0500

    Update Char.java

--- Char.java
+++ Char.java
@@ -1,1 +1,14 @@
+import java.util.Random;
+public class CountChar {
 
+    /**
+     * Count non space character in string
+     *
+     * @param str String to count the characters
+     * @return number of character in the specified string
+     */
+
+    public static int CountCharacters(String str) {
+        return str.replaceAll("\\s", "").length();
+    }
+}
