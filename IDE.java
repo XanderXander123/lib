f823141918eeaa23dd86f141d0f198c43af24194
_x128 <143216072+XanderXander123@users.noreply.github.com>
2023-09-20 04:26:25 -0500

    Delete IDE.java

--- IDE.java
+++
@@ -1,8 +0,0 @@
-package org.IDE.IDE;
-import java.awt.*;
-import java.awt.event.*;
-public class IDE {
-            private Frame frame; private TextArea textArea;
-            public IDE() { frame.setSize(800, 600); frame.addWindowListener(new WindowAdapter() { public void windowClosing(WindowEvent windowEvent) {
-            System.exit(0); }  }); textArea = new TextArea(); 
-            frame.add(textArea); frame.setVisible(true); } public static void main(String[] args) { new IDE(); }  }
